8f1045169b43e94272d0c0ab140986c072dba870
Ionut-Sorin Vladu <isvladu@proton.me>
2025-05-02 21:46:21 +0300

    Added the rest of the core tables required alongside 2 new link tables
    Signed-off-by: Ionut-Sorin Vladu <isvladu@proton.me>

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -42,7 +42,73 @@ pub struct User {
     #[unique]
     pub email: String,
     pub role: String, //this should be changed to an enum or fk to a different "Role" table
-    pub created_at: Timestamp,
-    pub updated_at: Timestamp,
+    pub created_at: u64,
+    pub updated_at: u64,
     pub profile_picture_url: Option<String>,
+}
+
+#[derive(Table, Serialize, Deserialize)]
+pub struct Course {
+    #[primarykey]
+    pub course_id: u64,
+    pub title: String,
+    pub description: String,
+    #[foreignkey(User)]
+    pub instructor_id: u64,
+    pub created_at: u64,
+    pub updated_at: u64,
+    pub status: String,
+}
+
+#[derive(Table, Serialize, Deserialize)]
+pub struct Module {
+    #[primarykey]
+    pub module_id: u64,
+    #[foreignkey(Course)]
+    pub course_id: u64,
+    pub title: String,
+    pub description: String,
+    pub order: u32,
+    pub created_at: u64,
+    pub updated_at: u64,
+}
+
+#[derive(Table, Serialize, Deserialize)]
+pub struct Lesson {
+    #[primarykey]
+    pub lesson_id: u64,
+    #[foreignkey(Module)]
+    pub module_id: u64,
+    pub title: String,
+    pub content: String,
+    pub lesson_type: String, //text/video/quiz/assignment - maybe change to Enum
+    pub order: u32,
+    pub created_at: u64,
+    pub updated_at: u64,
+}
+
+#[derive(Table, Serialize, Deserialize)]
+pub struct Enrollment {
+    #[primarykey]
+    pub enrollment_id: u64,
+    #[foreignkey(User)]
+    pub user_id: u64,
+    #[foreignkey(Course)]
+    pub course_id: u64,
+    pub enrolled_at: u64,
+    pub completion_status: String, // not_started/in_progress/completed
+    pub completed_at: Option<u64>,
+}
+
+#[derive(Table, Serialize, Deserialize)]
+pub struct UserProgress {
+    #[primarykey]
+    pub user_progress_id: u64,
+    #[foreignkey(User)]
+    pub user_id: u64,
+    #[foreignkey(Lesson)]
+    pub lesson_id: u64,
+    pub status: String, // not_started/started/completed
+    pub started_at: u64,
+    pub completed_at: Option<u64>,
 }
