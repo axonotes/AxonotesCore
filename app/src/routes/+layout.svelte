07f118b9c8ac8a211b657835c46341df9e7bfd27
Oliver Seifert <github.staging362@passmail.net>
2025-07-08 18:05:21 +0200

    feat: tailwindcss setup

---
+++ app/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import '../app.css';
+    import LightSwitch from "$lib/components/LightSwitch.svelte";
+	
+	let { children } = $props();
+</script>
+
+<div class="w-screen h-screen bg-red-500">
+    <LightSwitch/>
+    {@render children()}
+</div>
---
+++ app/src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<div class="bg-red-500 w-48 h-48">
+    test
+</div>
